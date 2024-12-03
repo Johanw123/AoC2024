4c115e95b937e78f32d458a2abf9b32e9193e39a
johanw1232 <73111635+johanw1232@users.noreply.github.com>
2024-12-03 13:35:22 +0100

    cleanup

---
+++ Day3/Program.cs
@@ -0,0 +1,14 @@
+﻿using System.Text.RegularExpressions;
+
+Console.WriteLine(Regex.Matches(File.ReadAllText("input.txt"), @"mul\((\d+),(\d+)\)").Aggregate(0, (acc, mul) => acc + int.Parse(mul.Groups[1].Value) * int.Parse(mul.Groups[2].Value)));
+
+bool doMul = true;
+Console.WriteLine(Regex.Matches(File.ReadAllText("input.txt"), @"mul\((\d+),(\d+)\)|do\(\)|don't\(\)").
+    Aggregate(0, (acc, mul) =>
+      {
+        if (mul.Value == "do()") doMul = true;
+        else if (mul.Value == "don't()") doMul = false;
+        else if (doMul) acc += int.Parse(mul.Groups[1].Value) * int.Parse(mul.Groups[2].Value);
+        return acc;
+      }));
+
