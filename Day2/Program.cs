2d994662525f75421735c02fb43b219c7b2bdd50
Johanw123 <johanw123@gmail.com>
2024-12-09 08:57:34 +0100

    Day 1 code

--- Day2/Program.cs
+++ Day2/Program.cs
@@ -6,31 +6,74 @@
   return safe;
 }));
 
-
-bool test = true;
 Console.WriteLine(File.ReadAllLines("input.txt").Aggregate(0, (safe, line) =>
 {
-  test = true;
-  if (line.Split(" ").Select(int.Parse).ToList().Aggregate(0, (acc, num) =>
-          { if ((acc == 0 && acc != int.MinValue) || (num < acc && Math.Abs(acc - num) <= 3)) acc = num; else { if (test) { test = false; } else { acc = int.MinValue; } }; return acc; }) != int.MinValue)
-  {
+  bool test = true;
+  if (line.Split(" ").Select(int.Parse).ToList().Aggregate(0, (acc, num) => { if (acc == 0 || num > acc && Math.Abs(acc - num) <= 3) acc = num; else acc = int.MaxValue; return acc; }) != int.MaxValue
+      || line.Split(" ").Select(int.Parse).ToList().Aggregate(0, (acc, num) => { if (acc == 0 || num < acc && Math.Abs(acc - num) <= 3) acc = num; else acc = int.MinValue; return acc; }) != int.MinValue)
     safe++;
-  }
   return safe;
 }));
 
-test = true;
-Console.WriteLine(File.ReadAllLines("input.txt").Aggregate(0, (safe, line) =>
-{
-  test = true;
-  if (line.Split(" ").Select(int.Parse).ToList().Aggregate(0, (acc, num) =>
-        { if ((acc == 0 && acc != int.MaxValue) || (num > acc && Math.Abs(acc - num) <= 3)) acc = num; else { if (test) { test = false; } else { acc = int.MaxValue; } }; return acc; }) != int.MaxValue)
+// bool test = true;
+// Console.WriteLine(File.ReadAllLines("input.txt").Aggregate(0, (safe, line) =>
+// {
+//   Console.WriteLine("Line: " + line);
+//   test = true;
+//   if (line.Split(" ").Select(int.Parse).ToList().Aggregate(0, (acc, num) =>
+//           { if (acc == 0 || (acc != int.MinValue && num < acc && Math.Abs(acc - num) <= 3)) acc = num; else { if (test) { test = false; } else { acc = int.MinValue; } }; Console.WriteLine(acc); return acc; }) != int.MinValue)
+//   {
+//     safe++;
+//
+//     Console.WriteLine("Its Safe!: " + safe);
+//   }
+//   return safe;
+// }));
 
-  {
-    safe++;
-  }
-  return safe;
-}));
+// bool test = true;
+// Console.WriteLine(File.ReadAllLines("input.txt").Aggregate(0, (safe, line) =>
+// {
+//
+//   test = true;
+//   bool s = false;
+//
+//   if (line.Split(" ").Select(int.Parse).ToList().Aggregate(0, (acc, num) =>
+//         { if (acc == 0 || (acc != int.MaxValue && num > acc && Math.Abs(acc - num) <= 3)) acc = num; else { if (test) { test = false; } else { acc = int.MaxValue; } }; return acc; }) != int.MaxValue)
+//
+//   {
+//     safe++;
+//     s = true;
+//   }
+//   else
+//   {
+//     if (line.Split(" ").Skip(1).Select(int.Parse).ToList().Aggregate(0, (acc, num) =>
+//         { if (acc == 0 || (acc != int.MaxValue && num > acc && Math.Abs(acc - num) <= 3)) acc = num; else { if (test) { test = false; } else { acc = int.MaxValue; } }; return acc; }) != int.MaxValue)
+//       safe++;
+//   }
+//
+//   test = true;
+//   if (line.Split(" ").Select(int.Parse).ToList().Aggregate(0, (acc, num) =>
+//           { if (acc == 0 || (acc != int.MinValue && num < acc && Math.Abs(acc - num) <= 3)) acc = num; else { if (test) { test = false; } else { acc = int.MinValue; } }; return acc; }) != int.MinValue)
+//   {
+//     safe++;
+//     s = true;
+//   }
+//   else
+//   {
+//     if (line.Split(" ").Skip(1).Select(int.Parse).ToList().Aggregate(0, (acc, num) =>
+//              { if (acc == 0 || (acc != int.MinValue && num < acc && Math.Abs(acc - num) <= 3)) acc = num; else { if (test) { test = false; } else { acc = int.MinValue; } }; return acc; }) != int.MinValue)
+//       safe++;
+//   }
+//
+//
+//   if (!s)
+//   {
+//
+//     Console.WriteLine("Line: " + line);
+//     Console.WriteLine("Not Safe!");
+//   }
+//   return safe;
+// }));
 
 
 
--- Day2/obj/Debug/net8.0/Day2.AssemblyInfo.cs
+++ Day2/obj/Debug/net8.0/Day2.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Day2")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+9b7e72891c8ea952eb3f149b2ae2ecc5d8844fa2")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+425b9e2bbf87bc811476dba13350c0f4cf651907")]
 [assembly: System.Reflection.AssemblyProductAttribute("Day2")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Day2")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
